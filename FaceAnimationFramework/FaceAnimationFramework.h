fbe0baa9b3cb1b7c1e8b3d67e5988d2e1e84f07d
Alex C <acoundou@gmail.com>
2023-09-10 00:07:49 -0400

    Trying to get Swift Package Manager to work...

---
+++ FaceAnimationFramework/FaceAnimationFramework.h
@@ -0,0 +1,18 @@
+//
+//  FaceAnimationFramework.h
+//  FaceAnimationFramework
+//
+//  Created by Alex Coundouriotis on 9/10/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FaceAnimationFramework.
+FOUNDATION_EXPORT double FaceAnimationFrameworkVersionNumber;
+
+//! Project version string for FaceAnimationFramework.
+FOUNDATION_EXPORT const unsigned char FaceAnimationFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FaceAnimationFramework/PublicHeader.h>
+
+
